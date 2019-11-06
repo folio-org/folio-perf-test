2bc4a345eb49c2280062afa1473651ed64714536
Hongwei Ji <hji@ebsco.com>
2019-11-06 16:47:56 -0500

    Use Okapi to get all modules

--- shared.groovy
+++ shared.groovy
@@ -146,7 +146,7 @@ def bootstrapModules(ctx) {
   sh "${ctx.scpCmd} folio-conf ${ctx.sshUser}@${ctx.dbIp}:/tmp"
 
   // def mods = getMods(ctx.fixedMods, ctx.stableFolio + "/okapi-install.json")
-  def mods = getMods(ctx.fixedMods, ctx.stableFolio.replaceFirst("\.", "-okapi.") + "/_/proxy/tenants/diku/modules")
+  def mods = getMods(ctx.fixedMods, ctx.stableFolio.replaceFirst("\\.", "-okapi.") + "/_/proxy/tenants/diku/modules")
   echo "mods: ${mods}"
   mods = registerMods(mods, ctx.mdRepo, ctx.okapiIp)
   echo "valid mods: ${mods}"
