5394b0fb744f0c55050b548a85d9bd434d5c2b17
Stanislav Miroshnichenko <celt@root.in.ua>
2020-08-17 15:57:54 +0300

    Changed Postgres max_connection parameter

--- config/db.sh
+++ config/db.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --name foliodb -e POSTGRES_USER=folio -e POSTGRES_PASSWORD=folioadmin -p5432:5432 postgres:10
+docker run -d --rm --name foliodb -e POSTGRES_USER=folio -e POSTGRES_PASSWORD=folioadmin -c max_connections=1000 -p5432:5432 postgres:10
