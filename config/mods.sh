62a49dde17f8408edc075ed9e27ce5ee9f088168
Mykola Borchuk <it.volu@gmail.com>
2021-03-16 12:33:20 +0200

    FOLIO-2982 Returned data

--- config/mods.sh
+++ config/mods.sh
@@ -1,1 +1,1 @@
-docker run -d --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/${modName}:${modVer} db_connection=/folio-conf/pg.json
+docker run -d --rm --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/${modName}:${modVer} db_connection=/folio-conf/pg.json
