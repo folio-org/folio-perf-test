13fb9c3ba82ffb4ad2b7b5e2c8acba30cb8043d7
abdenour-drif <78385054+abdenour-drif@users.noreply.github.com>
2021-04-29 14:25:41 +0300

    FOLIO-2982 updated mod-service-interaction.sh

--- config/mod-service-interaction.sh
+++ config/mod-service-interaction.sh
@@ -1,5 +1,2 @@
-docker run -d --rm --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/${modName}:${modVer}
-sleep 10
-docker container exec -it ${modName} /bin/bash
-db_connection=/folio-conf/pg.json
-exit
+docker run -d --rm --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/${modName}:${modVer} && docker container exec -it ${modName} /bin/bash && db_connection=/folio-conf/pg.jso && exit
+
