edb8d04d6f51ed2406c30a689a1c057ee5e58168
abdenour-drif <78385054+abdenour-drif@users.noreply.github.com>
2021-04-29 16:51:03 +0300

    FOLIO-2983 changed docker image for mod-service-interaction

--- config/mod-service-interaction.sh
+++ config/mod-service-interaction.sh
@@ -1,2 +1,1 @@
-docker run -d --rm --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/mod-courses:1.2.3-SNAPSHOT.117 db_connection=/folio-conf/pg.json
-
+docker run -d --rm --name mod-service-interaction --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi -p9230:8081 folioci/mod-service-interaction:1.0.0-SNAPSHOT.21  db_connection=/folio-conf/pg.json
