85861bafb3feaced0cce919a633b7b2700c8daa3
abdenour-drif <78385054+abdenour-drif@users.noreply.github.com>
2021-04-29 15:10:54 +0300

    FOLIO-2982 update script file

--- config/mod-service-interaction.sh
+++ config/mod-service-interaction.sh
@@ -1,1 +1,2 @@
-docker ps
+docker run -d --rm --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/mod-courses:1.2.3-SNAPSHOT.117 db_connection=/folio-conf/pg.json
+
