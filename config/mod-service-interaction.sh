a7a56ae9e4b7ac171554a9af79a8c2dfc9aaba6a
abdenour-drif <78385054+abdenour-drif@users.noreply.github.com>
2021-04-29 13:00:07 +0300

    FOLIO-2982 added run script for mod-service-interaction

---
+++ config/mod-service-interaction.sh
@@ -0,0 +1,1 @@
+docker run -d --rm --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/${modName}:${modVer} 
