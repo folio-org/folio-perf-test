b85e11208bc0e7cb237f90dbf597cd85a526db1f
abdenour-drif <78385054+abdenour-drif@users.noreply.github.com>
2021-08-16 23:25:27 +0300

    Update mod-inn-reach.sh

--- config/mod-inn-reach.sh
+++ config/mod-inn-reach.sh
@@ -1,1 +1,1 @@
-
+docker run -d --rm --name ${modName} --mount type=bind,source=/tmp/folio-conf,target=/folio-conf -e 'DB_HOST=${dbHost}' -e 'DB_PORT=5432' -e 'DB_DATABASE=folio' -e 'DB_USERNAME=folio' -e 'DB_PASSWORD=folioadmin' -e 'JAVA_OPTIONS=-Dorg.folio.metadata.inventory.storage.type=okapi' -p${port}:8081 folioci/${modName}:${modVer} db_connection=/folio-conf/pg.json
