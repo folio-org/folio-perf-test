dfda34a55410414272d23081854a56ecadba2998
Hleb Surnovich <Hleb_Surnovich@epam.com>
2021-09-16 19:11:16 +0300

    FOLIO-2982 added secret var for mod-kb-ebsco

--- shared.groovy
+++ shared.groovy
@@ -341,8 +341,12 @@ def runIntegrationTests(ctx) {
       mavenSettingsConfig: 'folioci-maven-settings'
     ) {
       def okapiDns = "ec2-" + ctx.okapiIp.replaceAll(/\./, "-") + ".compute-1.amazonaws.com"
-      withCredentials([usernamePassword(credentialsId: 'testrail-ut56', passwordVariable: 'testrail_password', usernameVariable: 'testrail_user')]) {
-      sh "mvn test -Dkarate.env=${okapiDns} -DfailIfNoTests=false -Dtestrail_url=${TestRailUrl} -Dtestrail_userId=${testrail_user} -Dtestrail_pwd=${testrail_password} -Dtestrail_projectId=${TestRailProjectId}"
+      withCredentials([usernamePassword(credentialsId: 'testrail-ut56', passwordVariable: 'testrail_password', usernameVariable: 'testrail_user'), string(credentialsId: 'mod-kb-ebsco-key', variable: 'ebsco_key')]) {
+      sh """
+      export kbEbscoCredentialsApiKey=${ebsco_key}
+      echo \$kbEbscoCredentialsApiKey
+      mvn test -Dkarate.env=${okapiDns} -DfailIfNoTests=false -Dtestrail_url=${TestRailUrl} -Dtestrail_userId=${testrail_user} -Dtestrail_pwd=${testrail_password} -Dtestrail_projectId=${TestRailProjectId}
+      """
       }
     }
     sh "mkdir ${env.WORKSPACE}/folio-integration-tests/cucumber-reports"
