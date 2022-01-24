bb807e8b1ffede5c7117966ab0c98db880d2061e
alehlitasau <92017954+alehlitasau@users.noreply.github.com>
2022-01-24 12:21:09 +0300

    RANCHER-146 Change branch for integration tests

--- shared.groovy
+++ shared.groovy
@@ -321,7 +321,7 @@ def runIntegrationTests(ctx) {
   echo "Checkout folio-integration-tests"
   checkout([
     $class: 'GitSCM',
-    branches: [[name: '*/master']],
+    branches: [[name: '*/RANCHER-146']],
     extensions: scm.extensions + [[$class: 'SubmoduleOption',
                                    disableSubmodules: false,
                                    parentCredentials: false,
