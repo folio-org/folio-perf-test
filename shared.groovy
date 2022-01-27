d9b7ac17f33d9ac167dd9e10c9440b450a9d867a
Maryia_Lazavik <Maryia_Lazavik@epam.com>
2022-01-27 19:34:41 +0300

    RANCHER-146 fix run integration test from master

--- shared.groovy
+++ shared.groovy
@@ -321,7 +321,7 @@ def runIntegrationTests(ctx) {
   echo "Checkout folio-integration-tests"
   checkout([
     $class: 'GitSCM',
-    branches: [[name: '*/RANCHER-146']],
+    branches: [[name: '*/master']],
     extensions: scm.extensions + [[$class: 'SubmoduleOption',
                                    disableSubmodules: false,
                                    parentCredentials: false,
